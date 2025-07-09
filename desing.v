485bbc33a591dbba442a19950eb367ea215383f9
LuixRom <luis.romero@utec.edu.pe>
2025-07-09 00:05:49 -0500

    Update desing.v

--- desing.v
+++ desing.v
@@ -102,7 +102,7 @@ module arm (
 		.ImmSrc(ImmSrc),
       	.ALUControl(ALUControl),
       	.lmulFlag(lmulFlag),
-      	.FpuWrite(FpuWrite)
+      	.FpuWrite(FpuWrite),
       	.RegSrcMul(RegSrcMul),
       	.mullargo(mullargo)
 	);
@@ -125,7 +125,7 @@ module arm (
 		.ImmSrc(ImmSrc),
       	.ALUControl(ALUControl),
       	.lmulFlag(lmulFlag),
-      	.FpuWrite(FpuWrite)
+      	.FpuWrite(FpuWrite),
       	.RegSrcMul(RegSrcMul),
       	.mullargo(mullargo)
 	);
@@ -571,7 +571,7 @@ module condlogic (
   	assign RegWrite[0] = RegW & actualCondEx;
   	assign RegWrite[1]  = RegW2  & actualCondEx;
     assign MemWrite = MemW & actualCondEx;
-  	assign FpuWrite = CondExFlop & FpuW;
+  	assign FpuWrite = actualCondEx & FpuW;
     assign PCSrc   = PCS & actualCondEx;
     assign PCWrite = PCSrc | NextPC;
   	
@@ -1173,4 +1173,36 @@ module fp_adder (
     wire [7:0]  expRshift = (sum[24]) ? (expR + 1) : expR;
 
     assign result = {srcA[31], expRshift, _sum[22:0]};
+endmodule
+
+
+module fpu_regfile (clk, we3, ra1, ra2, wa3, A1, A2, A3, sod, wd3, rd1, rd2);
+    input wire clk;
+    input wire we3;
+    input wire [3:0] ra1;
+    input wire [3:0] ra2;
+    input wire [3:0] wa3;
+    input wire A1;
+    input wire A2;
+    input wire A3;
+    input wire sod; 
+    input wire [63:0] wd3;
+    output wire [63:0] rd1;
+    output wire [63:0] rd2;
+    reg [63:0] rf [15:0];
+    always @(posedge clk)
+        if (we3)
+            if(sod == 1) 
+                rf[wa3] <= wd3;
+            else 
+                if(A3 == 1)
+                    rf[wa3][63:32] <= wd3[31:0];
+                else
+                    rf[wa3][31:0] <= wd3[31:0];
+
+    wire [31:0] rd1f = A1 == 1 ? rf[ra1][63:32] : rf[ra1][31:0];
+    wire [31:0] rd2f = A2 == 1 ? rf[ra2][63:32] : rf[ra2][31:0];
+
+    assign rd1 = sod == 1 ? rf[ra1] : {32'b0, rd1f};
+    assign rd2 = sod == 1 ? rf[ra2] : {32'b0, rd2f};
 endmodule
